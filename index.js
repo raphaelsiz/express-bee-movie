098bac89e110bde8de516a7c6d1e161cd69a726c
Raphael Sizemoore <rmorgansizemore@gmail.com>
2023-08-11 22:56:13 -0700

    made it send text instead of redirect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { redirect, condition } from "./config.js";
+import axios from "axios";
 
 const config = {
     redirect,
@@ -6,8 +7,11 @@ const config = {
 }
 export default function(options={}) {
     for (let prop in config) if (options[prop]) config[prop] = options[prop]
-    return function (req,res,next) {
-        if (config.condition(req)===true) res.redirect(config.redirect)
+    return async function (req,res,next) {
+        if (config.condition(req)===true) {
+            let script = await axios({method: 'get', url:config.redirect,responseType:"text"})
+            res.send(script.data)
+        }
         else next()
     }
 }
